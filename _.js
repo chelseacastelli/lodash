19e6da76db1e5688d453199dee02bdb0a3eae6f3
chelseacastelli <chel.castel@aol.com>
2021-06-04 11:14:09 -0400

    implement all number methods

--- _.js
+++ _.js
@@ -1,4 +1,23 @@
 
+const _ = {
+    clamp(number, lower, upper) {
+        const lowerClampedValue = Math.max(number, lower);
+        return Math.min(lowerClampedValue, upper);
+    },
+    inRange(number, start, end) {
+        if (!end) {
+            end = start;
+            start = 0;
+        } else if (start > end) {
+            const temp = start;
+            start = end;
+            end = temp;
+        }
+
+        return number < start || number >= end ? false : true;
+
+    }
+};
 
 
 
